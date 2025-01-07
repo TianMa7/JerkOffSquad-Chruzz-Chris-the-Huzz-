cef30d4ffb679cd90b98be556d293e8994164489
TianMa7 <tianma787@gmail.com>
2025-01-06 23:57:32 -0700

    Update SmallStage.java

--- src/Ex3_MusicFestival/SmallStage.java
+++ src/Ex3_MusicFestival/SmallStage.java
@@ -5,34 +5,32 @@ import java.util.concurrent.LinkedBlockingDeque;
 
 public class SmallStage extends Event{
 
-    ArrayList<String> techRequirements;
-    ArrayList<String> songs;
-    ArrayList<Integer> songGirth;
+    ArrayList<String> songNames;
+    int numSongs;
+
 
     public SmallStage(String name, String type){
         super(name, type);
-        super.setLocation("Sub Stage");
+        super.setLocation("Small Stage");
 
-        techRequirements = new ArrayList<String>();
-        songs = new ArrayList<String>();
-        songGirth = new ArrayList<Integer>();
-    }
-
-    public int numSongs = 0;
-
-    public void setSongTime() {
-        System.out.println("Whats the length of the suzz(song huzz)");
-        int girth = Library.input.nextInt();
-        Library.input.nextLine();
-        songGirth.add(girth);
-
+        this.songNames = new ArrayList<String>();
+        this.numSongs = 0;
 
     }
 
-    public void addSongs(){
-        System.out.println("On skibidi give me a song");
-        songs.add(Library.input.nextLine());
-        setSongTime();
-        numSongs += 1;
+    public void addSong(String song){
+        this.songNames.add(song);
+        this.numSongs++;
     }
+
+    public void printSongs(){
+        System.out.println("The songs for " + super.getEventName() + " are: ");
+        for(String song: this.songNames){
+            System.out.println(song);
+        }
+    }
+
+
+
+
 }
